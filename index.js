58f1264ae32a8b41654f3ff4fbc567ca5cca83df
Ashley <adunderwood96@gmail.com>
2022-01-18 13:48:39 -0500

    render the employee types and return html

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
+
+// Outside File Variables
+const Manager = require('./lib/Manager');
+const mgrQuestions = require('./promises/MgrQuestions');
+
+const Engineer = require('./lib/Engineer');
+const engQuestions = require('./promises/EngQuestions');
+
+const Intern = require('./lib/Intern');
+const internQuestions = require('./promises/InternQuestions');
+
+const render = require('./lib/htmlRenderer');
